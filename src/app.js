25803a7abce3b6575677c7d1bfca4e6313db0ecd
Rishabh TNT <rishabh221@gmail.com>
2026-04-29 08:44:55 +0530

    added user and video model

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
 
@@ -13,6 +14,6 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.use(cookieParse());
+app.use(cookieParser());
 
 export default app;
--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,5 @@
-import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
-
-dotenv.config();
 
 const dbConnection = async () => {
   try {
